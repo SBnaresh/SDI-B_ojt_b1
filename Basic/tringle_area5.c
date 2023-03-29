a60e0026f1d01d26d0d932679cc962982c0859ab
SBnaresh <114151582+SBnaresh@users.noreply.github.com>
2023-03-29 14:25:14 +0530

    Add files via upload

---
+++ Basic/tringle_area5.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+  int  main()
+  {
+    int a ,b, c;
+    printf("enter two angle of triagle");
+    scanf("%d %d",&a, &b);
+    c=180-(a+b);
+    printf("the area of triangle is c=%d",c);
+    return 0;
+    }
