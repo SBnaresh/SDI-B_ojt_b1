018f80d6e5ea7ba1f366999a69fc53a285c97b92
SBnaresh <114151582+SBnaresh@users.noreply.github.com>
2023-03-29 16:22:20 +0530

    Add files via upload

---
+++ 06_string/35_string.c
@@ -0,0 +1,37 @@
+/*#include<stdio.h>
+int main()
+{
+    char arr[20];
+printf("enter the aary element\n");
+gets(arr);
+printf("your string is\n");
+puts(arr);
+return 0;
+
+}*/
+
+
+/*#include<stdio.h>
+#include<string.h>
+#include<conio.h>
+int main() 
+{    
+    char s1[10]="hello"; 
+    char s2[10]="welcome";
+    puts(s1);
+    puts(s2);
+    strcpy(s1,s2);
+    puts(s1);
+    puts(s2);
+    return 0; */
+
+    #include<stdio.h>
+    int  main()
+    {
+         char name[24]="naresh meghwal";
+         char n2[23]="phytec ebeddd";
+         puts(name);
+         strrev(name);
+         puts(name);
+         return  0;
+    }
